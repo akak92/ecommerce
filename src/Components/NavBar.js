f1c98fdc7a5d7eee813ee547fbc53fe914461191
Pedro Diaz <pfdiaz@smn.gob.ar>
2024-10-28 02:41:29 -0300

    Agrego logo de tienda en navbar

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './NavBar.css';
-
+import logo from '../assets/logo_tienda.png';
 function NavBar() {
   return (
     <nav className="navbar">
-      <div className="logo">Mi Tienda</div>
+      <div className="logo">
+        <img src={logo} alt="Logo" className="logo-img" />
+        <span className="store-name"> MiTienda</span>
+      </div>
       <ul className="navLinks">
         <li><a href="#inicio" className="link">Inicio</a></li>
         <li><a href="#productos" className="link">Productos</a></li>
