545882a5ca9c3488f183c76822ce6d1cbeda73e2
Pedro Diaz <pfdiaz@smn.gob.ar>
2024-12-16 01:35:36 -0300

    Agrego renderizado condicional en carrito

--- src/Components/pages/Cart/Cart.js
+++ src/Components/pages/Cart/Cart.js
@@ -10,28 +10,31 @@ const Cart = () => {
     return (
         <div className="cart-container">
             <h2 className="cart-header">Carrito de compras</h2>
-            {cart.map(el => (
-                <div key={el.id} className="cart-item">
-                    <img src={el.img} alt={el.title} className="cart-item-image" />
-                    <div className="cart-item-details">
-                        <h3>{el.title}</h3>
-                        <h3>Cantidad: {el.quantity}</h3>
-                        <h3>Precio unitario: ${el.price}</h3>
+            {cart.length === 0 ? (
+                <h3 className="cart-header">El carrito está vacío</h3>
+            ) : (
+                <>
+                    {cart.map(el => (
+                        <div key={el.id} className="cart-item">
+                            <img src={el.img} alt={el.title} className="cart-item-image" />
+                            <div className="cart-item-details">
+                                <h3>{el.title}</h3>
+                                <h3>Cantidad: {el.quantity}</h3>
+                                <h3>Precio unitario: ${el.price}</h3>
+                            </div>
+                            <button onClick={() => removeProduct(el.id)}>Eliminar</button>
+                        </div>
+                    ))}
+                    <div className="cart-footer">
+                        <button onClick={resetCart}>Vaciar carrito</button>
+                        <Link to="/checkout">
+                            <button>Finalizar Compra</button>
+                        </Link>
+                        <h3>Total Compra: ${totalAmount}</h3>
                     </div>
-                    <button onClick={() => removeProduct(el.id)}>Eliminar</button>
-                </div>
-            ))}
-            {cart.length > 0 && (
-                <div className="cart-footer">
-                    <button onClick={resetCart}>Vaciar carrito</button>
-                    <Link to="/checkout">
-                        <button>Finalizar Compra</button>
-                    </Link>
-                    <h3>Total Compra: ${totalAmount}</h3>
-                </div>
+                </>
             )}
         </div>
     );
-};
-
+}
 export default Cart;
